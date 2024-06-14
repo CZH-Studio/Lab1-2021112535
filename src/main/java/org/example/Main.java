4999f010d49c5544228e471a44c5e238d991c8d3
QuTong Yi <czh666czh666czh666@163.com>
2024-06-14 11:36:51 +0800

    Pass checkstyle and spotbug.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+/**
+ * App start.
+ */
 public class Main {
-    public static void main(String[] args) {
-        /* B2 */
-        App.main(args);
-    }
-    /* This is a comment for verifying version: 1 */
-    /* 2021112535 */
+  public static void main(String[] args) {
+    /* B2 */
+    App.main(args);
+  }
+  /* This is a comment for verifying version: 1 */
+  /* 2021112535 */
 }
